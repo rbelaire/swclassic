2aae48650a127a7313835b496160716969ac1536
rbelaire <rjbelaire@gmail.com>
2026-01-29 14:38:16 -0600

    Add match rendering functionality in admin.js

---
+++ admin.js
@@ -0,0 +1,49 @@
+let data;
+
+fetch("./data.json")
+  .then(res => res.json())
+  .then(json => {
+    data = json;
+    render();
+  });
+
+function render() {
+  const container = document.getElementById("matches");
+  container.innerHTML = "";
+
+  data.matches.forEach(match => {
+    const div = document.createElement("div");
+    div.className = "match";
+
+    const usedPlayers = getUsedPlayers(match.round, match.id);
+
+    const p1 = buildPlayerSelect(match, 0, usedPlayers);
+    const p2 = buildPlayerSelect(match, 1, usedPlayers);
+
+    const swapBtn = document.createElement("button");
+    swapBtn.textContent = "Swap";
+    swapBtn.onclick = () => {
+      [match.playerIds[0], match.playerIds[1]] =
+        [match.playerIds[1], match.playerIds[0]];
+      render();
+    };
+
+    const clearBtn = document.createElement("button");
+    clearBtn.textContent = "Clear";
+    clearBtn.onclick = () => {
+      match.playerIds = [null, null];
+      render();
+    };
+
+    const title = document.createElement("h3");
+    title.textContent = `Match ${match.id} (Round ${match.round})`;
+
+    div.append(title, p1, p2, swapBtn, clearBtn);
+
+    if (!isValidMatchup(match)) {
+      div.classList.add("invalid");
+    }
+
+    container.appendChild(div);
+  });
+}
