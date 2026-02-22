83593d0080d3f9fdfeb0cd353d65a3d3dadfc3c8
Southline Studio <your@email.com>
2026-02-22 15:01:12 -0600

    Fix api/save.js to use CommonJS exports for Vercel

--- api/save.js
+++ api/save.js
@@ -7,7 +7,7 @@ const GITHUB_REPO = "rbelaire/swclassic";
 const GITHUB_BRANCH = "Main";
 const DATA_FILE_PATH = "data.json";
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
